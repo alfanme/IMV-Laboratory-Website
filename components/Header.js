4057ea3fbacbeeffd5d3540c31ed15c3c83ece37
Ahmad Alfan <aalf4nz@gmail.com>
2021-09-11 15:33:21 +0700

    Hide navigation menu temporarily

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
     return (
         <header className='sticky top-0 md:-top-12 z-50 flex justify-between items-center h-20 md:h-32 px-4 md:px-8 bg-white border-b border-gray-200'>
             <div className='sticky top-0 flex md:justify-between items-center h-20 w-full'>
-                <button
+                {/* <button
                     onClick={() => setShowMenu(prev => !prev)}
                     className='focus:outline-none block mr-4 md:hidden'>
                     {showMenu ? (
@@ -20,7 +20,7 @@ export default function Header() {
                     ) : (
                         <MenuIcon className='w-8 h-8' />
                     )}
-                </button>
+                </button> */}
                 <Image
                     src={Logo}
                     width={42}
@@ -31,7 +31,7 @@ export default function Header() {
                 <h1 className='hidden md:block mr-auto ml-4 text-lg font-medium tracking-wider uppercase'>
                     IMV Laboratory
                 </h1>
-                <nav>
+                {/* <nav>
                     <ul
                         className={`fixed top-20 left-0 bottom-0 ${
                             showMenu ? 'translate-x-0' : '-translate-x-full'
@@ -62,7 +62,7 @@ export default function Header() {
                             </Link>
                         </li>
                     </ul>
-                </nav>
+                </nav> */}
                 <button
                     onClick={() => router.push('/webinar')}
                     className='flex items-center ml-auto md:ml-16 px-4 h-10 rounded-lg bg-blue-500 text-white hover:bg-blue-700 focus:outline-none'>
