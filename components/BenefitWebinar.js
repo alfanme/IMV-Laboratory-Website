37b9cb2852a742f45ca13753569915fbf9938a14
Ahmad Alfan <aalf4nz@gmail.com>
2021-09-12 19:03:26 +0700

    Fix flex layout auto wrap

--- components/BenefitWebinar.js
+++ components/BenefitWebinar.js
@@ -11,8 +11,8 @@ export default function BenefitWebinar() {
             <h1 className='text-2xl font-bold mb-8'>
                 Apa aja sih "benefit" ikutan webinar ini?
             </h1>
-            <div className='flex flex-col md:flex-row justify-center items-center gap-8 md:gap-16'>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+            <div className='flex flex-wrap justify-center items-center gap-8'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <AcademicCapIcon className='w-32 h-32 text-pink-300 mb-2' />
                     <p className='font-semibold text-lg'>
                         Pemahaman Data Science
@@ -22,7 +22,7 @@ export default function BenefitWebinar() {
                         Scientist profesional.
                     </p>
                 </div>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <LightBulbIcon className='w-32 h-32 text-yellow-300 mb-2' />
                     <p className='font-semibold text-lg'>
                         Percakapan Inspiratif
@@ -32,7 +32,7 @@ export default function BenefitWebinar() {
                         Data Science.
                     </p>
                 </div>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <BadgeCheckIcon className='w-32 h-32 text-blue-300 mb-2' />
                     <p className='font-semibold text-lg'>E-Sertifikat</p>
                     <p className='text-sm mt-2 text-gray-500'>
@@ -40,7 +40,7 @@ export default function BenefitWebinar() {
                         masukkan ke CV.
                     </p>
                 </div>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <UserGroupIcon className='w-32 h-32 text-purple-300 mb-2' />
                     <p className='font-semibold text-lg'>Networking</p>
                     <p className='text-sm mt-2 text-gray-500'>
--- components/WebinarPrograms.js
+++ components/WebinarPrograms.js
@@ -6,8 +6,8 @@ export default function WebinarPrograms() {
             <h1 className='text-2xl font-bold mb-8'>
                 Kegiatan seru pada webinar:
             </h1>
-            <div className='flex flex-col md:flex-row justify-center items-center gap-8 md:gap-16'>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+            <div className='flex flex-wrap justify-center items-center gap-8'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <ChatAlt2Icon className='w-32 h-32 text-yellow-300 mb-2' />
                     <p className='font-semibold text-lg'>Talkshow</p>
                     <p className='text-sm mt-2 text-gray-500'>
@@ -16,7 +16,7 @@ export default function WebinarPrograms() {
                         dalam kehidupan sehari-hari.
                     </p>
                 </div>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <PuzzleIcon className='w-32 h-32 text-pink-300 mb-2' />
                     <p className='font-semibold text-lg'>Games Berhadiah</p>
                     <p className='text-sm mt-2 text-gray-500'>
@@ -24,7 +24,7 @@ export default function WebinarPrograms() {
                         dengan narasumber. Sssttt, ada hadiahnya loh!
                     </p>
                 </div>
-                <div className='flex flex-col items-center px-8 md:px-0 md:w-56'>
+                <div className='flex flex-col flex-1 min-w-[15em] max-w-[20em] items-center px-8 md:px-0'>
                     <GiftIcon className='w-32 h-32 text-blue-300 mb-2' />
                     <p className='font-semibold text-lg'>Penawaran Spesial</p>
                     <p className='text-sm mt-2 text-gray-500'>
