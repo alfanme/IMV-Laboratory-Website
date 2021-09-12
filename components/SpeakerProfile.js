7fb0914b764bec2f1165ae6072418d72cb2c79d3
Ahmad Alfan <aalf4nz@gmail.com>
2021-09-12 19:04:12 +0700

    Give gradient touch ring around the photo

--- components/SpeakerProfile.js
+++ components/SpeakerProfile.js
@@ -7,15 +7,20 @@ export default function SpeakerProfile({
     otherDescription,
 }) {
     return (
-        <div className='text-center'>
-            <Image
-                src={imageURL}
-                width={150}
-                height={150}
-                objectFit='contain'
-                className='rounded-full'
-            />
-            <div className='mt-4'>
+        <div className='flex flex-col items-center'>
+            <div className='flex justify-center items-center h-[170px] w-[170px] rounded-full bg-gradient-to-r from-blue-400 via-pink-400 to-yellow-400'>
+                <div className='flex justify-center items-center h-[160px] w-[160px] rounded-full bg-white'>
+                    <Image
+                        src={imageURL}
+                        width={150}
+                        height={150}
+                        objectFit='cover'
+                        className='rounded-full'
+                    />
+                </div>
+            </div>
+
+            <div className='mt-4 text-center'>
                 <p className='text-lg font-bold'>{name}</p>
                 <p className='text-sm font-semibold text-blue-500'>
                     {position}
